3d6abe26779c69c5c03443ab7433528dd04f687e
Joaci Queiroz <jcqroz@gmail.com>
2024-05-15 21:54:16 -0300

    faet: Atualização classe contador

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -3,19 +3,31 @@ import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro");
-        int parametroDois = terminal.nextInt();
         
-        try {
-            // chamando o método contendo a lógica de contagem
-            contar(parametroUm, parametroDois);
-        } catch (ParametrosInvalidosException e) {
-            // imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-            System.out.println(e.getMessage());
+        int tentativasRestantes = 3; // Iniciamos com três tentativas, incluindo a atual
+        
+        while (tentativasRestantes > 0) {
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
+            
+            try {
+                // chamando o método contendo a lógica de contagem
+                contar(parametroUm, parametroDois);
+                break; // Se a contagem for bem-sucedida, saímos do loop
+            } catch (ParametrosInvalidosException e) {
+                // imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+                System.out.println(e.getMessage());
+                
+                tentativasRestantes--;
+                if (tentativasRestantes > 0) {
+                    System.out.println("Você tem mais " + tentativasRestantes + " tentativas.");
+                } else {
+                    System.out.println("Você não tem mais tentativas. Encerrando o programa.");
+                }
+            }
         }
-        
     }
 
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
